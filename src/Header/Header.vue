54c6b1ecaa4c15a3f0013c84472837af88e399f3
Henrikh Abajyan <h.abajyan@gmail.com>
2017-09-28 15:52:03 +0400

    Header setups

--- src/Header/Header.vue
+++ src/Header/Header.vue
@@ -1,8 +1,30 @@
 <template>
-  <footer class="Header">
-    I'm Header
-  </footer>
+
+  <header class="Header">
+    <div class="Container">
+
+      <div class="HeaderGroup">
+        <div class="Logo">
+          <img src=""> {jsconf.am}
+        </div>
+        <div class="Nav">
+          <nav class="Nav">
+            <a href="#">HOME</a>
+            <a href="#">SPEAKERS</a>
+            <a href="#">SCHEDULE</a>
+            <a href="#">SPONSERS</a>
+            <a href="#">SHORTCODES</a>
+          </nav>
+        </div>
+
+      </div>
+
+    </div>
+  </header>
+
 </template>
+
+
 
 <script>
 export default {
@@ -10,10 +32,35 @@ export default {
 };
 </script>
 
-<style>
-.Header {
 
-  background-color: #ccc;
+
+<style>
+
+.Header {
+  display: flex;
+  height: 64px;
+  background-color: black;
+  justify-content: center;
+  align-items: center;
+}
+
+.Header .HeaderGroup {
+  display: flex;
+  height: 64px;
+  width: 1170px;
+  background-color: rgba(255, 255, 255, 0.8);
+  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
 
 }
+
+.Nav a {
+  color: #ccc;
+}
+
+.Logo {
+  color: #ccc;
+}
+
 </style>
