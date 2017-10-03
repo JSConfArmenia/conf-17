372f4f6286c4796f2c43aa7b39d1a8cb20782ef3
Henrikh Abajyan <h.abajyan@gmail.com>
2017-10-04 00:48:29 +0400

    Speakers Section - Done

--- src/Location/Location.vue
+++ src/Location/Location.vue
@@ -3,6 +3,7 @@
     <iframe
       class="Map"
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3048.1640706992257!2d44.52429321526298!3d40.18316197939297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abd4e4c4a93a9%3A0xb1a196eecbc8204e!2sInnovative+Solutions+and+Technologies+Center+(ISTC)!5e0!3m2!1sen!2s!4v1506956160551" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
   </section>
 </template>
 
--- src/Speakers/Speakers.vue
+++ src/Speakers/Speakers.vue
@@ -1,19 +1,171 @@
 <template>
   <section class="Speakers">
-    I'm speakers section
+    <div class="Container">
+
+      <h1 class="Title">WHO'S NINJAS?</h1>
+      <div class="row">
+             <!-- NINJA-1  -->
+        <div class="col-md-3">
+          <div class="Img i1"></div>
+          <a href=""><h3 class="Name">Rouben Meschian</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-2  -->
+        <div class="col-md-3">
+          <div class="Img i2"></div>
+          <a href=""><h3 class="Name">Gevorg Harutyunyan</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-3  -->
+        <div class="col-md-3">
+          <div class="Img i3"></div>
+          <a href=""><h3 class="Name">Shahen Hovhannisyan</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-4  -->
+        <div class="col-md-3">
+          <div class="Img i4"></div>
+          <a href=""><h3 class="Name">Edgar Marukyan</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-5  -->
+        <div class="col-md-3">
+          <div class="Img i5"></div>
+          <a href=""><h3 class="Name">Name</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-6  -->
+        <div class="col-md-3">
+          <div class="Img i6"></div>
+          <a href=""><h3 class="Name">Name</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-7  -->
+        <div class="col-md-3">
+          <div class="Img i7"></div>
+          <a href=""><h3 class="Name">Name</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+             <!-- NINJA-8  -->
+        <div class="col-md-3">
+          <div class="Img i8"></div>
+          <a href=""><h3 class="Name">Name</h3></a>
+          <h5 class="Position">Position</h5>
+        </div>
+      </div>
+
+    </div>
   </section>
 </template>
 
 <script>
+
 export default {
 
 };
 </script>
 
-<style>
+<style scoped>
+
+.i1 {
+  background-image: url(../_assets/RoubenMeschian.jpg);
+}
+
+.i2 {
+  background-image: url(../_assets/GevorgHarutyunyan.jpg);
+}
+
+.i3 {
+  background-image: url(../_assets/ShahenHovhannisyan.jpg);
+}
+
+.i4 {
+  background-image: url(../_assets/EdgarMarukyan.jpg);
+}
+
+.i5 {
+  background-image: url(../_assets/NINJA1.png);
+}
+
+.i6 {
+  background-image: url(../_assets/widget-pattern.png);
+}
+
+.i7 {
+  background-image: url(../_assets/NINJA1.png);
+}
+
+.i8 {
+  background-image: url(../_assets/widget-pattern.png);
+}
+
+.Title {
+  color: #fff;
+  text-align: center;
+}
+
+.Name {
+  color: #fff;
+}
+
+a {
+  text-decoration: none;
+}
+
+h3:hover {
+  color: #4bb463;
+}
+
+.Position {
+  color: #fff;
+  margin-bottom: 20px;
+}
+
 .Speakers {
+  background-image: url(../_assets/who-is-speaking-2.png);
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 50px 0 50px 0;
+}
 
-  background-color: #ccc;
+.Img {
+  padding-bottom: 100%;
+  margin-bottom: 10px;
+  background-size: cover;
+  border: 1px solid #ccc;
+}
 
+.i1 {
+  background-image: url(../_assets/RoubenMeschian.jpg);
+}
+
+.i2 {
+  background-image: url(../_assets/GevorgHarutyunyan.jpg);
+}
+
+.i3 {
+  background-image: url(../_assets/ShahenHovhannisyan.jpg);
+}
+
+.i4 {
+  background-image: url(../_assets/EdgarMarukyan.jpg);
+}
+
+.i5 {
+  background-image: url(../_assets/NINJA1.png);
+}
+
+.i6 {
+  background-image: url(../_assets/widget-pattern.png);
+}
+
+.i7 {
+  background-image: url(../_assets/NINJA1.png);
+}
+
+.i8 {
+  background-image: url(../_assets/widget-pattern.png);
 }
 </style>
