d2fa692d08cad6e1a6222bc7a06c749fe2ed6cc7
Henrikh Abajyan <h.abajyan@gmail.com>
2017-10-04 15:50:07 +0400

    Sponsors Section - Done

--- src/Sponsors/Sponsors.vue
+++ src/Sponsors/Sponsors.vue
@@ -2,8 +2,55 @@
   <section class="Sponsors" id="Sponsors">
     <div class="Container">
 
-      Sponsors
+      <h1 class="Title">Sponsors and friends</h1>
+      <div class="row">
 
+        <!-- Sponsor 1 -->
+        <div class="col">
+          <div
+            class="Img"
+            style="background-image: url(https://scontent.fevn1-2.fna.fbcdn.net/v/t1.0-9/10407244_810068769065474_484284539248915848_n.png?oh=b8c7ca57d379b75ae320e4a8ae14d064&oe=5A7A916E)">
+          </div>
+          <h4>Simply Technologies</h4>
+        </div>
+
+        <!-- Sponsor 2 -->
+        <div class="col">
+          <div
+            class="Img"
+            style="background-image: url(https://scontent.fevn1-2.fna.fbcdn.net/v/t31.0-8/21731750_1736095493088418_8372728911051007100_o.jpg?oh=a8f84a82b7ac16c17efd0a2fb30f5141&oe=5A876DAA)">
+          </div>
+          <h4>Innovative Solutions and Technologies Center</h4>
+        </div>
+
+        <!-- Sponsor 3 -->
+        <div class="col">
+          <div
+            class="Img"
+            style="background-image: url(https://scontent.fevn1-2.fna.fbcdn.net/v/t31.0-8/21992996_345238609249586_7860761651385358583_o.jpg?oh=9fa447152c0f3a33091613815805c7a0&oe=5A4D6B1E)">
+          </div>
+          <h4>Iterate Hackerspace</h4>
+        </div>
+
+        <!-- Sponsor 4 -->
+        <div class="col">
+          <div
+            class="Img"
+            style="background-image: url(https://raw.githubusercontent.com/modularcode/modularcode.github.io/master/images/logo.png)">
+          </div>
+          <h4>ModularCode</h4>
+        </div>
+
+        <!-- Sponsor 5 -->
+        <div class="col">
+          <div
+            class="Img"
+            style="background-image: url(http://onesportsnation.com/goodyear/wp-content/uploads/sites/2/2016/06/sponsor.jpg)">
+          </div>
+          <h4>Become a sponsor</h4>
+        </div>
+
+    </div>
     </div>
   </section>
 </template>
@@ -18,8 +65,32 @@ export default {
 <style scoped>
 
 .Sponsors {
-  background-color: #fff;
-  height: 800px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-image: url(http://demo.themeum.com/wordpress/eventum/wp-content/uploads/2015/09/home-sponsors-bg.jpg?id=152);
 }
 
+.Img {
+  padding-bottom: 100%;
+  margin-bottom: 10px;
+  background-size: cover;
+  border: 1px solid #ccc;
+  border-radius: 100%;
+  background-size: cover;
+  background-position: center;
+}
+
+h1 {
+  color: #00ac97;
+  text-align: center;
+  padding: 45px 0 45px 0;
+}
+
+h4 {
+  color: #00ac97;
+  text-align: center;
+  margin-bottom: 60px;
+  padding-top: 10PX;
+}
 </style>
