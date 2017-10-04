7f5b207200ac21cb583c8e47717820a154265ca8
Henrikh Abajyan <h.abajyan@gmail.com>
2017-10-04 17:28:02 +0400

    Sponsors
    UI adjustments

--- src/Sponsors/Sponsors.vue
+++ src/Sponsors/Sponsors.vue
@@ -2,7 +2,7 @@
   <section class="Sponsors" id="Sponsors">
     <div class="Container">
 
-      <h1 class="Title">Sponsors and friends</h1>
+      <h3 class="Title">Sponsors and friends</h3>
       <div class="row">
 
         <!-- Sponsor 1 -->
@@ -12,7 +12,7 @@
               class="Img"
               style="background-image: url(https://scontent.fevn1-2.fna.fbcdn.net/v/t1.0-9/10407244_810068769065474_484284539248915848_n.png?oh=b8c7ca57d379b75ae320e4a8ae14d064&oe=5A7A916E)">
             </div>
-            <h5>Simply Technologies</h5>
+            <h5 class="OrgName">Simply Technologies</h5>
           </a>
         </div>
 
@@ -23,7 +23,7 @@
               class="Img"
               style="background-image: url(https://scontent.fevn1-2.fna.fbcdn.net/v/t31.0-8/21731750_1736095493088418_8372728911051007100_o.jpg?oh=a8f84a82b7ac16c17efd0a2fb30f5141&oe=5A876DAA)">
             </div>
-            <h5>Innovative Solutions and<br />Technologies Center</h5>
+            <h5 class="OrgName">Innovative Solutions and<br />Technologies Center</h5>
           </a>
         </div>
 
@@ -34,7 +34,7 @@
               class="Img"
               style="background-image: url(http://iteratehackerspace.com/static/images/logo.png)">
             </div>
-            <h5>Iterate Hackerspace</h5>
+            <h5 class="OrgName">Iterate Hackerspace</h5>
           </a>
         </div>
 
@@ -45,7 +45,7 @@
               class="Img"
               style="background-image: url(https://raw.githubusercontent.com/modularcode/modularcode.github.io/master/images/logo.png)">
             </div>
-            <h5>ModularCode</h5>
+            <h5 class="OrgName">ModularCode</h5>
           </a>
         </div>
 
@@ -67,6 +67,7 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   background-image: url(http://demo.themeum.com/wordpress/eventum/wp-content/uploads/2015/09/home-sponsors-bg.jpg?id=152);
+  padding: 50px 0 50px;
 }
 
 .Img {
@@ -78,16 +79,16 @@ export default {
   margin: 0 auto;
 }
 
-h1 {
+.Title {
   color: #92278f;
   text-align: center;
-  padding: 45px 0 45px 0;
+  padding-bottom: 30px;
 }
 
-h5 {
+.OrgName {
   color: #666;
   text-align: center;
-  margin-bottom: 60px;
+  margin-bottom:20px;
   padding-top: 10PX;
 }
 
